1b12bd0db8ef774fb7347d1fc096774c14fbf3ad
Prerit <32776569+prer1t@users.noreply.github.com>
2019-06-18 11:38:19 +0530

    Rename dataset_2.py to dataset_2_.py

---
+++ dataset_2_.py
@@ -0,0 +1,15 @@
+while(True):
+    ret, img = cam.read()
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = detector.detectMultiScale(gray, 1.3, 5)
+    for (x,y,w,h) in faces:
+        cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)         
+        sampleNum=sampleNum+1
+        cv2.imwrite("dataset/label."+Id +'.'+ str(sampleNum) + ".jpg", gray[y:y+h,x:x+w])
+        cv2.imshow('frame',img)
+    if cv2.waitKey(100) & 0xFF == ord('q'):
+        break
+    elif sampleNum>500:
+        break
+cam.release()
+cv2.destroyAllWindows()
